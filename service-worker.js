1a34b2ca040837f34e4ca3669376768f02b017a0
cherent tsegaye <abenigas59@gmail.com>
2025-05-22 13:30:03 +0300

    minor update

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,28 @@
-const CACHE_NAME = "audio-cache-v1";
-const AUDIO_URL = "https://santimbingo.duckdns.org/assets/start.m4a";
+// const CACHE_NAME = "audio-cache-v1";
+// const AUDIO_URL = "https://santimbingo.duckdns.org/assets/start.m4a";
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.add(AUDIO_URL);
-    })
-  );
-});
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache.add(AUDIO_URL);
+//     })
+//   );
+// });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.url === AUDIO_URL) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        return (
-          cachedResponse ||
-          fetch(event.request).then((response) => {
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, response.clone());
-              return response;
-            });
-          })
-        );
-      })
-    );
-  }
-});
+// self.addEventListener("fetch", (event) => {
+//   if (event.request.url === AUDIO_URL) {
+//     event.respondWith(
+//       caches.match(event.request).then((cachedResponse) => {
+//         return (
+//           cachedResponse ||
+//           fetch(event.request).then((response) => {
+//             return caches.open(CACHE_NAME).then((cache) => {
+//               cache.put(event.request, response.clone());
+//               return response;
+//             });
+//           })
+//         );
+//       })
+//     );
+//   }
+// });
