3a6d3251b8b80662f67002c1b4242fb3447b2bbd
cherent tsegaye <abenigas59@gmail.com>
2025-05-26 16:42:32 +0300

    minir

--- server.js
+++ server.js
@@ -1385,7 +1385,7 @@ Alltime balance :  Br. ${balance}  \n  \`\`\``,
       const [userId, amount] = depositeData.split("_");
       console.log("User ID:", userId);
       console.log("amount:", otherId);
-      bot.sendMessage(adminUser, "userid" + userId + "amount " + amount);
+      bot.sendMessage(adminUser, "got it");
       // Query DB or perform action with userId
       break;
     default:
