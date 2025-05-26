6e6db4ab7d0d7f7c277c43a537780b914d06de49
cherent tsegaye <abenigas59@gmail.com>
2025-05-26 17:16:46 +0300

    minir

--- server.js
+++ server.js
@@ -1409,22 +1409,15 @@ Alltime balance :  Br. ${balance}  \n  \`\`\``,
 bot.onText(/\/balance/, (msg) => {
   const chatId = msg.chat.id;
   const telegramIdd = msg.from.id;
-  db.get(
-    "SELECT balance FROM users WHERE telegram_id = ?",
-    [telegramIdd],
-    async (err, row) => {
-      if (err || !row) {
-        console.error("DB error:", err);
-        bot.sendMessage(
-          chatId,
-          "❌ Could not fetch balance. Please try again."
-        );
-      }
-      bot.sendMessage(chatId, "Balance: Br. ");
-      // console.log(row);
-      return;
+  db.get("SELECT balance FROM users", [telegramIdd], async (err, row) => {
+    if (err || !row) {
+      console.error("DB error:", err);
+      bot.sendMessage(chatId, "❌ Could not fetch balance. Please try again.");
     }
-  );
+    bot.sendMessage(chatId, "Balance: Br. ");
+    // console.log(row);
+    return;
+  });
 });
 
 bot.onText(/\/invite/, (msg) => {
